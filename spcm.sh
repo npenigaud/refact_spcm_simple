0de4ed9fb62490a1a678811392601177f67996cc
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2023-02-10 13:13:19 +0000

    More test data

--- spcm.sh
+++ spcm.sh
@@ -12,8 +12,9 @@ cd /home/gmap/mrpm/marguina/pack/48t3_sidyn-spcm.05.IMPIIFC2018.x/spcm_simple
 \rm *.grb
 
 #xport MPIAUTOCONFIG=mpiauto.DDT.conf
-#/opt/softs/mpiauto/mpiauto --verbose -np 8 --wrap --wrap-stdeo -- ./spcm.x --case t0031l015-8mpi --write-grib-1 --write-grib-2 --stat
-/opt/softs/mpiauto/mpiauto --verbose -np 8 --wrap --wrap-stdeo -- ./spcm.x --case t0107l070-8mpi --stat
+#/opt/softs/mpiauto/mpiauto --verbose -np 8 --wrap --wrap-stdeo -- ./spcm.x --case t0031l015-008mpi --write-grib-1 --write-grib-2 --stat
+#/opt/softs/mpiauto/mpiauto --verbose -np 8 --wrap --wrap-stdeo -- ./spcm.x --case t0107l070-008mpi --stat
+ /opt/softs/mpiauto/mpiauto --verbose -np 1 --wrap --wrap-stdeo -- ./spcm.x --case t0031l015-001mpi --stat
 
 
 
