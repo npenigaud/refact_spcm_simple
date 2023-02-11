8d6a40a22743f43f563e6aabf45b24cb475888c5
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2023-02-11 11:14:17 +0000

    Add --stat-sp

--- spcm.sh
+++ spcm.sh
@@ -6,15 +6,19 @@
 
 set -x
 
+ulimit -s unlimited
+export OMP_STACK_SIZE=4G
+
 cd /home/gmap/mrpm/marguina/pack/48t3_sidyn-spcm.05.IMPIIFC2018.x/spcm_simple
 
 
 \rm *.grb
 
 #xport MPIAUTOCONFIG=mpiauto.DDT.conf
-#/opt/softs/mpiauto/mpiauto --verbose -np 8 --wrap --wrap-stdeo -- ./spcm.x --case t0031l015-008mpi --write-grib-1 --write-grib-2 --stat
-#/opt/softs/mpiauto/mpiauto --verbose -np 8 --wrap --wrap-stdeo -- ./spcm.x --case t0107l070-008mpi --stat
- /opt/softs/mpiauto/mpiauto --verbose -np 1 --wrap --wrap-stdeo -- ./spcm.x --case t0031l015-001mpi --stat
+#/opt/softs/mpiauto/mpiauto --verbose -np 8 --wrap --wrap-stdeo -- ./spcm.x --case t0031l015-008mpi --write-grib-1 --write-grib-2 --stat-gp
+#/opt/softs/mpiauto/mpiauto --verbose -np 8 --wrap --wrap-stdeo -- ./spcm.x --case t0107l070-008mpi --stat-gp
+#/opt/softs/mpiauto/mpiauto --verbose -np 1 --wrap --wrap-stdeo -- ./spcm.x --case t0031l015-001mpi --stat-gp
 
 
+ /opt/softs/mpiauto/mpiauto --verbose -np 8 -openmp 1 --wrap --wrap-stdeo -- ./spcm.x --case t0149l105-008mpi --write-grib-1 --write-grib-2 --stat-gp --stat-sp
 
