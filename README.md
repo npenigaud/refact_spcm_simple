15c3a0156148fa9ad490f0ef4b4c4d62f8f90c69
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2023-03-22 15:21:32 +0000

    Add images, etc.

---
+++ README.md
@@ -0,0 +1,9 @@
+# spcm_simple.F90
+
+Simplified SI scheme of ARPEGE/IFS.
+
+![](./images/SPCM_SIMPLE.svg)
+
+# Temperature correction T149L105
+
+![](./images/snapshot_0004.png)
--- spcm.sh
+++ spcm.sh
@@ -15,10 +15,10 @@ cd /home/gmap/mrpm/marguina/pack/48t3_sidyn-spcm.05.IMPIIFC2018.x/spcm_simple
 \rm *.grb
 
 #xport MPIAUTOCONFIG=mpiauto.DDT.conf
-#/opt/softs/mpiauto/mpiauto --verbose -np 8 --wrap --wrap-stdeo -- ./spcm.x --case t0031l015-008mpi --write-grib-1 --write-grib-2 --stat-gp
-#/opt/softs/mpiauto/mpiauto --verbose -np 8 --wrap --wrap-stdeo -- ./spcm.x --case t0107l070-008mpi --stat-gp
-#/opt/softs/mpiauto/mpiauto --verbose -np 1 --wrap --wrap-stdeo -- ./spcm.x --case t0031l015-001mpi --stat-gp
+ /opt/softs/mpiauto/mpiauto --verbose -np 8 --wrap --wrap-stdeo -- ./intel/spcm.x --case t0031l015-008mpi --write-grib-1 --write-grib-2 --stat-gp
+#/opt/softs/mpiauto/mpiauto --verbose -np 8 --wrap --wrap-stdeo -- ./intel/spcm.x --case t0107l070-008mpi --stat-gp
+#/opt/softs/mpiauto/mpiauto --verbose -np 1 --wrap --wrap-stdeo -- ./intel/spcm.x --case t0031l015-001mpi --stat-gp
 
 
- /opt/softs/mpiauto/mpiauto --verbose -np 8 -openmp 1 --wrap --wrap-stdeo -- ./spcm.x --case t0149l105-008mpi --write-grib-1 --write-grib-2 --stat-gp --stat-sp
+#/opt/softs/mpiauto/mpiauto --verbose -np 8 -openmp 1 --wrap --wrap-stdeo -- ./intel/spcm.x --case t0149l105-008mpi --write-grib-1 --write-grib-2 --stat-gp --stat-sp
 
